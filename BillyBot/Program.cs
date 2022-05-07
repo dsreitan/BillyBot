3f141e986e3fba98b752510052296bf41ff3a77d
Dagfinn Reitan <dagfinn.reitan@gyldendal.no>
2022-05-07 09:58:39 +0200

    Added BillyBot project

---
+++ BillyBot/Program.cs
@@ -0,0 +1,22 @@
+﻿using SC2APIProtocol;
+using Sharky;
+using Sharky.DefaultBot;
+// using SharkyProtossExampleBot;
+
+var gameConnection = new GameConnection();
+var defaultSharkyBot = new DefaultSharkyBot(gameConnection);
+
+var protossBuildChoices = new ProtossBuildChoices(defaultSharkyBot);
+defaultSharkyBot.BuildChoices[Race.Protoss] = protossBuildChoices.BuildChoices;
+
+var sharkyExampleBot = defaultSharkyBot.CreateBot(defaultSharkyBot.Managers, defaultSharkyBot.DebugService);
+
+var myRace = Race.Protoss;
+if (args.Length == 0)
+{
+    gameConnection.RunSinglePlayer(sharkyExampleBot, @"2000AtmospheresAIE.SC2Map", myRace, Race.Random, Difficulty.VeryHard, AIBuild.RandomBuild).Wait();
+}
+else
+{
+    gameConnection.RunLadder(sharkyExampleBot, myRace, args).Wait();
+}
