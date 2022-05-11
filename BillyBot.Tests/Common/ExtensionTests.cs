d76f0af5457ea741f8d6f7bc2a9e6094df129622
Dagfinn Reitan <dagfinn.reitan@gyldendal.no>
2022-05-11 21:27:25 +0200

    Added UnitTests

---
+++ BillyBot.Tests/Common/ExtensionTests.cs
@@ -0,0 +1,45 @@
+using System.Linq;
+using AutoFixture;
+using BillyBot.Common;
+using NUnit.Framework;
+using SC2APIProtocol;
+using Sharky;
+
+namespace BillyBot.Tests.Common;
+
+public class ExtensionTests
+{
+    private ActiveUnitData? _activeUnitData;
+    private SharkyOptions? _sharkyOptions;
+    private SharkyUnitData? _sharkyUnitData;
+    private UnitDataService? _unitDataService;
+
+    [SetUp]
+    public void Setup()
+    {
+        var fixture = new Fixture();
+        fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+            .ForEach(b => fixture.Behaviors.Remove(b));
+        fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+        _sharkyUnitData = fixture.Create<SharkyUnitData>();
+        _sharkyOptions = fixture.Create<SharkyOptions>();
+        _activeUnitData = fixture.Create<ActiveUnitData>();
+        _unitDataService = new(_sharkyUnitData, _sharkyOptions, new());
+    }
+
+    [Theory]
+    [TestCase(.10f, ExpectedResult = true)]
+    [TestCase(.60f, ExpectedResult = false)]
+    public bool IsUnitNearlyCompleted(float percentCompleteThreshold)
+    {
+        // setup
+        var unitType = UnitTypes.PROTOSS_ZEALOT;
+        var unit = new Unit {UnitType = (uint) unitType, BuildProgress = .25f, Pos = new() {X = 1, Y = 2}};
+        _sharkyUnitData.UnitData.Add(unitType, new());
+        _activeUnitData.SelfUnits.TryAdd(1, new(unit, 0, _sharkyUnitData, _sharkyOptions, _unitDataService, 1));
+
+        // assert
+        return _activeUnitData.CompletedAndNearlyCompleted(UnitTypes.PROTOSS_ZEALOT, percentCompleteThreshold) > 0;
+    }
+}
